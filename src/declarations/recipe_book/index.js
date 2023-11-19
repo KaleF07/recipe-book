5e69f3b9cfe5e5a458814492a6858e502a2e43a3
Kale <kale.fordham@ucalgary.ca>
2023-11-18 22:11:59 -1000

    Added canister id

--- src/declarations/recipe_book/index.js
+++ src/declarations/recipe_book/index.js
@@ -4,7 +4,7 @@ import { Actor, HttpAgent } from "@dfinity/agent";
 import { idlFactory } from './recipe_book.did.js';
 export { idlFactory } from './recipe_book.did.js';
 // CANISTER_ID is replaced by webpack based on node environment
-export const canisterId = process.env.RECIPE_BOOK_CANISTER_ID;
+export const canisterId = j5oby-gaaaa-aaaag-acbhq-cai;
 
 /**
  * 
